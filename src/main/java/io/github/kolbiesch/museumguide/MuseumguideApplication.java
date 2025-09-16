baab7a1ad09435708364d2e83cc8e52b03ec2f98
Kolbi Esch <kesch010@gmail.com>
2025-09-16 19:38:25 -0400

    Initial Spring Boot project setup with Web, JPA, Security, Validation, Lombok, and Flyway

---
+++ src/main/java/io/github/kolbiesch/museumguide/MuseumguideApplication.java
@@ -0,0 +1,13 @@
+package io.github.kolbiesch.museumguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuseumguideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuseumguideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/kolbiesch/museumguide/MuseumguideApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.kolbiesch.museumguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuseumguideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
