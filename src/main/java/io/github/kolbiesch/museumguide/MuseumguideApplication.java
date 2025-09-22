21afedb9f5c4a3ed77b4e7dc84a6bc4f2d522811
Kolbi Esch <kesch010@gmail.com>
2025-09-22 16:20:44 -0400

    Enable JPA repositories in main

--- src/main/java/io/github/kolbiesch/museumguide/MuseumguideApplication.java
+++ src/main/java/io/github/kolbiesch/museumguide/MuseumguideApplication.java
@@ -2,8 +2,10 @@ package io.github.kolbiesch.museumguide;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "io.github.kolbiesch.museumguide.repository")
 public class MuseumguideApplication {
 
 	public static void main(String[] args) {
