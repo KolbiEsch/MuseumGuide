54422bec9eb5ceeb759042ad32e3e9e91f9e1392
Kolbi Esch <kesch010@gmail.com>
2025-09-29 20:05:06 -0400

    Added missing getId to Exhibit entity

--- src/main/java/io/github/kolbiesch/museumguide/entities/Exhibit.java
+++ src/main/java/io/github/kolbiesch/museumguide/entities/Exhibit.java
@@ -73,6 +73,7 @@ public class Exhibit {
                         .orElse(null));
     }
 
+    public Long getId() { return this.id; }
     public void setId(Long id) { this.id = id; }
 
     public String getName() { return name; }
