916599f5b79ca669a010630c1b36d66c629eb2fa
Kolbi Esch <kesch010@gmail.com>
2026-01-06 11:17:26 -0500

    Create CommentService

--- src/main/java/io/github/kolbiesch/museumguide/entities/Comment.java
+++ src/main/java/io/github/kolbiesch/museumguide/entities/Comment.java
@@ -1,10 +1,13 @@
 package io.github.kolbiesch.museumguide.entities;
 
 import jakarta.persistence.*;
+import lombok.Builder;
+
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "comments")
+@Builder
 public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
